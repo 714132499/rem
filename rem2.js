ae1586d2f33ddd68ade183c1b9ee33d5716dd70c
BraveSuperman <15603833035@163.com>
2019-07-04 09:23:51 +0800

    Add files via upload

---
+++ rem2.js
@@ -0,0 +1,14 @@
+    <script>
+        function setRootFontSize() {
+            var a = document.documentElement, b = a.getBoundingClientRect().width || screen.width || 320;
+            a.style.fontSize = (640 >= b ? b : 640) / 3.2 + "px"
+        }
+
+        setRootFontSize();
+        window.addEventListener("resize", setRootFontSize, !1);
+    </script>
+
+
+
+
+≥ﬂ¥Á≥˝“‘200 µ√rem÷µ
