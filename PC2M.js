bbb7fdc83905b2ec42291bc567b9d85f6a5815a6
BraveSuperman <15603833035@163.com>
2019-11-13 09:36:58 +0800

    Add files via upload
    PC端判断是否是移动端，跳转移动端页面链接

---
+++ PC2M.js
@@ -0,0 +1,4 @@
+(function (a, b) {
+    if ((/AppleWebKit.*Mobile/i.test(a) || /Android|Windows Phone|webOS|iPhone|iPod|BlackBerry/i.test(a) || /MIDP|SymbianOS|NOKIA|SAMSUNG|LG|NEC|TCL|Alcatel|BIRD|DBTEL|Dopod|PHILIPS|HAIER|LENOVO|MOT-|Nokia|SonyEricsson|SIE-|Amoi|ZTE/.test(a)) && location.hash != '#m') window.location = b
+})(navigator.userAgent, '//www.uguoba.com/guoba-sc/special/bim20/bim-sj/index.html')
+//判断是否是 移动端，并跳转的移动端链接
